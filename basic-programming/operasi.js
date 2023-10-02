8cf3a5e99d1ed6568701f48877a881325a866e62
alimul <alimuldev@gmail.com>
2023-10-02 17:11:28 +0700

    add

---
+++ basic-programming/operasi.js
@@ -0,0 +1,10 @@
+let angks = "5" + "4"
+console.log(angks)
+
+
+
+// buat variable angka1 isinya 12 dan angka2 isnya 7. lalu buat variable angka3 yang isinya penjumlahan angka1 dan angka2
+let angka1 = 12
+let angka2 = 7
+let angka3 = angka1 + angka2
+console.log(angka3)
